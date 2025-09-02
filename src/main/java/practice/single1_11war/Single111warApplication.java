0369f1827f1e6d7266b714b7fd12219941a1ffe3
Beny <qkdehdgur3@gmail.com>
2025-09-02 15:20:49 +0900

    프젝 시작

---
+++ src/main/java/practice/single1_11war/Single111warApplication.java
@@ -0,0 +1,13 @@
+package practice.single1_11war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Single111warApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Single111warApplication.class, args);
+    }
+
+}
---
+++ src/test/java/practice/single1_11war/Single111warApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.single1_11war;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Single111warApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
